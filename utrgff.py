5ef8d8c2a07cf93d76b49b4dedb77adeca9c424b
matidae <matidae@gmail.com>
2013-05-07 14:05:51 -0300

    fix

--- utrgff.py
+++ utrgff.py
@@ -33,7 +33,7 @@ def printHeader(gff_file, contigs):
 def getGenes(gff_file):
     genes = []
     for line in gff_file:
-        if "CDS" in line:
+        if "\tCDS\t" in line:
             genes.append(Gene(contig=line.split()[0], source=line.split()[1],
                               key=line.split()[2], start=int(line.split()[3]),
                               end=int(line.split()[4]), strand=line.split()[6],
@@ -54,7 +54,7 @@ def getPrevNextGene(gen, genes, contig):
         n += 1
         if g.name == gen.name:
             if n == 1:
-                return (Gene(start=0, end=0), genes[1])
+                return (Gene(start=0, end=0), genes[0])
             elif n == int(len(genes)):
                 return (genes[n - 1], Gene(start=contig.end, end=contig.end))
             else:
@@ -64,42 +64,48 @@ def getPrevNextGene(gen, genes, contig):
 def checkOverlap(gen, prevnextgene, utr, cutoff, contig):
     prevgene = prevnextgene[0]
     nextgene = prevnextgene[1]
-    if gen.start > prevgene.end:
-        if gen.strand == "+":
-            label = "3UTR"
+#    print vars(prevnextgene[0])
+#    print vars(prevnextgene[1])
+#    print "--------------------------------------"
+    if gen.strand == "+":
+        label5 = "5UTR"
+        label3 = "3UTR"
+    if gen.strand == "-":
+        label3 = "5UTR"
+        label5 = "3UTR"
+
+    if gen.start - utr > prevgene.end + utr:
+        printline(gen, gen.start - utr - 1, label5, gen.strand)
+    elif gen.start - utr <= 0 :
+        utraux = 1
+        printline(gen, utraux, label5, gen.strand)
+    else:
+        utraux = int(math.ceil((prevgene.end - gen.start) / 2))
+        if utraux > cutoff:
+            printline(gen, gen.start - utraux - 1, label5, gen.strand)
         else:
-            label = "5UTR"
-        if gen.start - utr > prevgene.end + utr:
-            printline(gen, gen.start - utr, label, gen.strand)
+            pass
+    if gen.end + utr < nextgene.start - utr:
+        printline(gen, gen.end + utr + 1, label3, gen.strand)
+    elif gen.end + utr >= contig.end :
+        utraux = contig.end
+        printline(gen, utraux, label3, gen.strand)
+    else:
+        utraux = int(math.floor((nextgene.start - gen.end) / 2))
+        if utraux > cutoff:
+            printline(gen, gen.end + utraux, label3, gen.strand)
         else:
-            if prevgene.end == 0:
-                utraux = gen.start - 1
-            else:
-                utraux = int(math.ceil(((gen.start - prevgene.end) * .5)))
-            if utraux > cutoff:
-                printline(gen, gen.start - utraux, label, gen.strand)
-    if not gen.end >= nextgene.start and gen.end <= nextgene.end:
-        if gen.strand == "+":
-            label = "3UTR"
-        else: 
-            label = "5UTR"
-        if gen.end + utr < nextgene.start - utr:
-            printline(gen, gen.end + utr, label, gen.strand)
-        else:
-            if nextgene.start == contig.end:
-                utraux = contig.end - gen.end
-            else:
-                utraux = int(((nextgene.start - gen.end) * .5) - 1)
-            if utraux > cutoff:
-                printline(gen, gen.end + utraux, label, gen.strand)
+            pass
 
 
 def getUTRs(gen, utr, genes, contig, cutoff):
     if contig.name == gen.contig and gen.strand == "+":
-        prevnextgene = getPrevNextGene(gen, genes, contig)
+        genescontig = [g for g in genes if g.contig == contig.name ]
+        prevnextgene = getPrevNextGene(gen, genescontig, contig)
         checkOverlap(gen, prevnextgene, utr, cutoff, contig)
     if contig.name == gen.contig and gen.strand == "-":
-        prevnextgene = getPrevNextGene(gen, genes, contig)
+        genescontig = [g for g in genes if g.contig == contig.name ]
+        prevnextgene = getPrevNextGene(gen, genescontig, contig)
         checkOverlap(gen, prevnextgene, utr, cutoff, contig)
 
 
@@ -107,15 +113,16 @@ if __name__ == "__main__":
     gff_file = open(sys.argv[1], 'r').readlines()
     utr = 200
     cutoff = 20
-    if len(sys.argv) > 2: 
+    if len(sys.argv) > 2:
         utr = int(sys.argv[2])
     if len(sys.argv) > 3:
         cutoff = int(sys.argv[3])
     contigs = getContigs(gff_file)
     printHeader(gff_file, contigs)
     genes = getGenes(gff_file)
-    genespos = [g for g in genes if g.strand == "+"]
-    genesneg = [g for g in genes if g.strand == "-"]
+    genespos = sorted([g for g in genes if g.strand == "+"], key = lambda x : x.start)
+    genesneg = sorted([g for g in genes if g.strand == "-"], key = lambda x : x.start)
+
     for g in genes:
         print "\t".join(map(str, [g.contig, g.source, g.key, g.start, g.end, ".", g.strand, ".", "ID=" + g.name]))
         cont = [c for c in contigs if c.name == g.contig][0]
